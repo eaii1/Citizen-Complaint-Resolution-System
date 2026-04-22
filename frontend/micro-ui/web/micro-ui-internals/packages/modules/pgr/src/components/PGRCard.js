d97a228a7051bcc1d240726f5da19b3a94452e79
mersh101 <meraolfeye6@gmail.com>
2026-04-22 11:33:21 +0200

    fix the postal display config work correctly

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/components/PGRCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/components/PGRCard.js
@@ -35,7 +35,7 @@ const PGRCard = () => {
 
   let links = [
     generateLink("ACTION_TEST_CREATE_COMPLAINT", "create-complaint", ["CSR"]),
-    generateLink("ACTION_TEST_SEARCH_COMPLAINT", "inbox-v2"),
+    generateLink("ACTION_TEST_SEARCH_COMPLAINT", "inbox"),
   ];
   const hasRequiredRoles = (link) => { 
     if (!link?.roles?.length) return true;
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/configs/CreateComplaintConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/pgr/src/configs/CreateComplaintConfig.js
@@ -207,6 +207,23 @@ export const CreateComplaintConfig = {
             },
           ],
         },
+        {
+  "head": "CS_ADDCOMPLAINT_UPLOAD_PHOTO",
+  "body": [
+    {
+      "type": "component",
+      "isMandatory": false,
+      "component": "SelectImages",
+      "key": "ComplaintImagesPoint",
+      "label": "CS_ADDCOMPLAINT_UPLOAD_PHOTO_TEXT",
+      "populators": {
+        "name": "ComplaintImagesPoint"
+      }
+    },
+
+  ]
+
+},
 
       ],
     }
